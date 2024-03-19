1471dd42466e48d87992ad9b9d49e488c9ee94b5
Sean <ksw6125000@naver.com>
2024-03-19 17:29:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ markdown_text = """
   <a href="https://sean-lets-go.tistory.com/"><img src="https://img.shields.io/badge/Tech%20Blog-11B48A?style=flat-square&logo=Vimeo&logoColor=white&link=https://sienna1022.tistory.com/"/></a>&nbsp
   <a href="mailto:ksw6125000@gmail.com"><img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=20200803@gmail.com"/></a>
 </p>
+<br/>
 
-<br>
 ## ✅ Latest Blog Post
 
 """  # list of blog posts will be appended here
